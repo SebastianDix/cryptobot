4851156da076fe41f52ec59bc1df6b09c17db865
SebastianDix <mail@lektorpraha.cz>
2021-06-30 11:06:39 +0200

    able to get total CZK value of my Binance wallet

--- app.py
+++ app.py
@@ -6,34 +6,93 @@ from numpy import genfromtxt
 import talib
 from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
 from pprint import pprint as pp
+from pprint import pformat as pf
 from flask import Flask, render_template, url_for
 from operator import itemgetter # helps sort list of tuples faster than lambda, see https://stackoverflow.com/questions/3121979/how-to-sort-a-list-tuple-of-lists-tuples-by-the-element-at-a-given-index
+from forex_python.converter import CurrencyRates
 
-api_key=os.environ.get('api_key')
-api_secret=os.environ.get('api_secret')
-client = Client(api_key, api_secret)
+import time
+start_time = time.time()
 
 app = Flask(__name__)
+def vdir(obj):
+    return pf([x for x in dir(obj) if not x.startswith('__')])
 
-# tickers = 
-# [{'price': '0.05547700', 'symbol': 'ETHBTC'},
-#   {'price': '0.00383300', 'symbol': 'LTCBTC'},
-#  {'price': '0.00827400', 'symbol': 'BNBBTC'},
-tickers = client.get_all_tickers()
-def getEurPriceForAsset(assetName,tickers):
-    for ticker in tickers:
-        if ticker['symbol'] == assetName+"EUR":
-            return ticker['price']
-        else:
-            return None
+def get_Eur_Czk():
+    return CurrencyRates().get_rate('EUR','CZK')
 
 
+class Binance:
+    def __repr__(self):
+        return "Binance object: " + vdir(self)
 
-balances = client.get_account()['balances']
-balances = [(b['asset'],float(b['free'])) for b in balances if float(b['free']) != float(0)]
-balances.sort(key=itemgetter(2))
-print(balances)
+    def __init__(self):
+        api_key=os.environ.get('api_key')
+        api_secret=os.environ.get('api_secret')
+        self.client = Client(api_key, api_secret)
 
+    def get_all_balances(self):
+        return self.client.get_account()['balances']
+
+    def get_all_tickers(self):
+        return self.client.get_all_tickers()
+
+
+
+class Wallet:
+    def __init__(self,binance=Binance()):
+        self.binance = binance
+
+    def gen_balances(self):
+        for balance in self.binance.get_all_balances():
+            s_asset = balance['asset']
+            f_free = float(balance['free'])
+            f_locked = float(balance['locked'])
+            if f_free != 0 or f_locked != 0:
+                yield (s_asset,f_free+f_locked)
+
+    def get_usdt_values(self):
+        values_usdt = []
+        balance_dict=dict(self.gen_balances())
+        asset_tuple=tuple(a+"USDT" for a in balance_dict.keys())
+        # only get prices for assets we actually have
+        for ticker in self.binance.get_all_tickers():
+            pair_symbol = ticker['symbol']
+            symbol = pair_symbol[:-4]
+            price = float(ticker['price'])
+            if pair_symbol == "EURUSDT":
+                self.eurusdt = price
+            if pair_symbol in asset_tuple:
+                usdt_balance = balance_dict[symbol] * price
+                values_usdt.append((symbol,usdt_balance))
+        if balance_dict.get('USDT'):
+            values_usdt.append(("USDT",balance_dict['USDT']))
+        return values_usdt
+
+    def get_eur_values(self):
+        to_eur = lambda t: (t[0], t[1] / self.eurusdt)
+        return list(map(to_eur,self.get_usdt_values()))
+    
+    def get_czk_values(self):
+        eur_czk = float(get_Eur_Czk())
+        to_czk = lambda t: (t[0], t[1] * eur_czk)
+        return list(map(to_czk,self.get_eur_values()))
+    
+    def get_total_czk_value(self):
+        return sum(map(lambda x: x[1],self.get_czk_values()))
+
+
+pp(Wallet().get_total_czk_value())
+
+# pp(list(gen_assets(balances)))
+# pp(list(get_usdt_values(tickers)))
+
+# print(tuple(asset for asset in showNonZeroBalances(balances)))
+# getEurPriceForAsset("EOS",tickers=tickers)
+
+# balances.sort(key=itemgetter(2))
+
+print(round(time.time() - start_time,6), "seconds")
 exit()
 
 for b in balances:
---
+++ plan.md
@@ -0,0 +1,31 @@
+% Plan for this crypto bot 
+========================
+** Abbreviations:
+* UVC = understanding, viewing, configuring
+
+# Part 0, Goals
+===============
+
+1. See all my holdings in CZK and total portfolio value in CZK and how it has been doing.
+========================================================================================
+	* So take all tickers.
+	* Convert them to some stable coin.
+	* Convert stable coin to eur
+	* Convert EUR to CZK based on curren interest rate.
+
+2. Make a graph of that
+3. Perhaps graph historical transcation data and what you did and where you went wrong.
+
+
+# Part 1, UVC account data
+==========================
+
+# Part 2, UVC trading data (candles,depth?,order book?, line?)
+==============================================================
+
+# Part 3, UVC indicators 
+=========================
+
+# Part 4, UVC external signals (Twitter API? Musk? Bezos? Trending?)
+===================================================================
+
